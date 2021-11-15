5d9624b9543ff8e04016f18961fa0ba1c544507b
67254856 <73210068+67254856@users.noreply.github.com>
2021-11-15 21:36:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -94,13 +94,6 @@ function draw(){
     ctx.fillRect(i * width, GAME_HEIGHT - elements[i],
       width,elements[i]);
     ctx.fill();  
-    cvs.beginPath(); /* 図形を描き始めることを宣言 */
-  cvs.moveTo(50, 50); /* 図形の描き始めを移動 */
-  cvs.lineTo(150, 50); /* 図形の線の終わりを決める */
-  cvs.lineTo(150, 150);
-  cvs.lineTo(50, 150);
-  cvs.closePath(); /* 描いた線を閉じる */
-  cvs.stroke(); /* 描いた図形を線で表示させる */
   }
 
 }
